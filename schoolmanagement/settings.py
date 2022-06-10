42124bb89c9531f31a529ea538e96f1475bce8a1
NameUzer <wpalabay_20UR1772@psu.edu.ph>
2022-06-10 08:50:05 +0800

    commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,7 +133,7 @@ STATICFILES_DIRS=[
 STATIC_DIR,
  ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGIN_REDIRECT_URL='/afterlogin'
 
 
