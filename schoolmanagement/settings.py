d01077faf30d7512bb863c30228bae57fef2dbf8
NameUzer <wpalabay_20UR1772@psu.edu.ph>
2022-06-13 00:12:02 +0800

    commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'school/static')
 MEDIA_URL = '/images/'
 STATICFILES_DIRS=[
 STATIC_DIR,
